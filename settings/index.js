753013a7e0de8a0f2ad2afa5b3ab223b8246d76f
Luis Vilches <luis.vilches@cardumen.cl>
2019-05-25 01:36:36 -0400

    port

--- settings/index.js
+++ settings/index.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     SERVER:{
-        port: process.env.PORT || 5000,
+        port: process.env.PORT || 12001,
         static_url: "static",
         media_url: "media",
         cors:{
